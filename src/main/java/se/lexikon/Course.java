b96b901f5280399bdc68a82ccfadb9107fa62092
Kioskpeda <alle_yeah@live.se>
2020-12-11 11:55:27 +0100

    qeqweqw

---
+++ src/main/java/se/lexikon/Course.java
@@ -0,0 +1,61 @@
+package se.lexikon;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Course {
+    private int id;
+    private String courseName;
+    private LocalDate startDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getWeekDuration() {
+        return weekDuration;
+    }
+
+    public void setWeekDuration(int weekDuration) {
+        this.weekDuration = weekDuration;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    private int weekDuration;
+    private List<Student> students;
+
+    public void register(Student student) {
+
+    }
+    public void unregister(Student student){
+
+    }
+
+}
