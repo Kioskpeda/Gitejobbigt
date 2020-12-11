e33e931fbec398ad1949b34581b755e964e69733
Kioskpeda <alle_yeah@live.se>
2020-12-11 12:25:18 +0100

    Daos?

---
+++ src/main/java/se/lexikon/data_access/CourseDao.java
@@ -0,0 +1,4 @@
+package se.lexikon.data_access;
+
+public interface CourseDao {
+}
---
+++ src/main/java/se/lexikon/data_access/StudentDao.java
@@ -0,0 +1,18 @@
+package se.lexikon.data_access;
+
+import se.lexikon.Student;
+
+import java.util.List;
+
+public interface StudentDao {
+
+    Student saveStudent (Student student);
+    Student findByEmail(String email);
+    List<Student> findByName(String name);
+    Student findById(int id);
+    List<Student>findAll();
+    boolean deleteStudent(Student student);
+
+
+
+}
