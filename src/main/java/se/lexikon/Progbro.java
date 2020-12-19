5c2949a6deef3b3193c88c7583a9749111a12437
Kioskpeda <alle_yeah@live.se>
2020-12-19 14:23:41 +0100

    Daos?

---
+++ src/main/java/se/lexikon/Progbro.java
@@ -0,0 +1,27 @@
+package se.lexikon;
+
+import se.lexikon.data_access.CourseDaoList;
+import se.lexikon.data_access.StudentDaoList;
+
+public class Progbro {
+    CourseDaoList torsten = new CourseDaoList();
+
+    public void createCourse(){
+        Course kek = new Course();
+       kek.setId();
+       kek.setStartDate();
+       kek.setWeekDuration();
+        torsten.saveCourse(kek);
+    }
+    StudentDaoList flink = new StudentDaoList();
+
+    public void createStudent(){
+        Student kek = new Student();
+        kek.setId();
+        kek.setEmail();
+        kek.setAddress();
+        kek.setName();
+        flink.saveStudent(kek);
+    }
+
+}
